43dbfd277f74871b6a048c96bcb94dd8d1c526bb
MJ Johnson <mjohnson4685@gmail.com>
2020-09-14 06:58:45 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,17 +12,17 @@ end
 # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
 def shipping_manifest
 the_manifest = {
-  "whale bone corsets" = 5,
-  "porcelain vases" = 2,
-  "oil paintings" = 3
+  "whale bone corsets" => 5,
+  "porcelain vases" => 2,
+  "oil paintings" => 3
     }
 end
 
 def retrieval
  shipping_manifest = {
-    "whale bone corsets" = 5,
-    "porcelain vases" = 2,
-    "oil paintings" = 3
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
     }
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
   shipping_manifest["oil paintings"]
@@ -30,8 +30,8 @@ end
 
 def adding
   shipping_manifest = {
-      "whale bone corsets" = 5,
-      "porcelain vases" = 2,
+      "whale bone corsets" => 5,
+      "porcelain vases" => 2,
       "oil paintings" = 3
                  }
   # add 2 muskets to the shipping_manifest hash below
